bb87b47093ac5e0d5678f79e60164b07c25d1636
rajesh <rajesh.t7982@gmail.com>
2026-02-09 15:42:36 +0530

    v2

---
+++ APIs/AdminAPI.js
@@ -0,0 +1,6 @@
+import exp from 'express'
+export const  adminRoute=exp.Router()
+
+//Authenticate admin
+//Read all articles
+//Block / unblock user roles
---
+++ APIs/UserAPI.js
@@ -0,0 +1,33 @@
+import exp from "express";
+import { register, authenticate } from "../services/authService.js";
+
+export const userRoute = exp.Router();
+
+//Register user
+userRoute.post("/users", async (req, res) => {
+  //get user obj from req
+  let userObj = req.body;
+  //call register
+  const newUserObj = await register({ ...userObj, role: "USER" });
+  //send res
+  res.status(201).json({ message: "user created", payload: newUserObj });
+});
+
+//Authenticate user
+userRoute.post("/authenticate", async (req, res) => {
+  //get user cred object
+  let userCred = req.body;
+  //call authenticate service
+  let { token, user } = await authenticate(userCred);
+  //save tokan as httpOnly cookie
+  res.cookie("token", token, {
+    httpOnly: true,
+    sameSite: "lax",
+    secure: false,
+  });
+  //send res
+  res.status(200).json({ message: "login success", payload: user });
+});
+
+//Read all articles(protected route)
+//Add comment to an article(protected route)
